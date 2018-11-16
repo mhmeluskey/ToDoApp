6dd44d23f1ae72e5e857b5455ade9ec99cb1aed5
Megan <mhmeluskey@gmail.com>
2018-11-16 11:20:42 -0500

    handlebars display data

--- models/todo.js
+++ models/todo.js
@@ -5,7 +5,12 @@ var todo = {
         orm.all("items", function(res) {
             cb(res); 
         });
-    }
+    },
+    create: function(cols, vals, cb) {
+        orm.create("cats", cols, vals, function(res) {
+          cb(res);
+        });
+      }
 }
 
 module.exports = todo;
